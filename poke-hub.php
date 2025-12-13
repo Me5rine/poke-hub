b78073bb5f7569e13ca703a765c5919fd96bc6fd
Me5rine <admin@me5rine-lab.com>
2025-12-13 01:46:45 +0100

    test: deploy poke-hub on both sites

--- poke-hub.php
+++ poke-hub.php
@@ -10,7 +10,7 @@ Text Domain: poke-hub
 Domain Path: /languages
 */
 
-// Deploy test: poke-hub auto-deploy OK
+// Deploy test: poke-hub auto-deploy OK or NOT
 
 if (!defined('ABSPATH')) {
     exit;
